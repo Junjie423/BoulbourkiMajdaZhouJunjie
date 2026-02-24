7d65dc758a55f93fae26dc9c7528d0369631bbd3
Junjie423 <jzhouzho423@alumnes.ub.edu>
2026-02-24 13:56:31 +0100

    Initial Version

--- src/prog2/model/LlistaReserves.java
+++ src/prog2/model/LlistaReserves.java
@@ -1,4 +1,37 @@
 package prog2.model;
 
-public class LlistaReserves {
+import prog2.vista.ExcepcioReserva;
+
+import java.time.LocalDate;
+
+public class LlistaReserves implements InLlistaReserves{
+    // Falta atributs i constructor
+
+
+    /**
+     * Comprova que l'estada que es demani sigui més llarga o igual que l'estada mínima.
+     * Comprova que l'allotjament estigui disponible pels dies indicats.
+     * En cas afirmatiu, crea la reserva i l’afegeix a la llista de reserves del camping.
+     * En cas negatiu, llança una excepció de tipus ExceptionReserva amb el missatge d'error.
+     *
+     * @param allotjament
+     * @param client
+     * @param dataEntrada
+     * @param dataSortida
+     * @throws ExcepcioReserva
+     */
+    @Override
+    public void afegirReserva(Allotjament allotjament, Client client, LocalDate dataEntrada, LocalDate dataSortida) throws ExcepcioReserva {
+
+    }
+
+    /**
+     * Retorna el número de reserves de la llista.
+     *
+     * @return el número de reserves.
+     */
+    @Override
+    public int getNumReserves() {
+        return 0;
+    }
 }
--- src/prog2/model/Reserva.java
+++ src/prog2/model/Reserva.java
@@ -1,4 +1,47 @@
 package prog2.model;
 
-public class Reserva {
+import java.time.LocalDate;
+
+public class Reserva implements InReserva {
+    //Faltan atributos y constructror
+
+    @Override
+    public Allotjament getAllotjament_() {
+        return null;
+    }
+
+    @Override
+    public Client getClient() {
+        return null;
+    }
+
+    @Override
+    public LocalDate getDataEntrada() {
+        return null;
+    }
+
+    @Override
+    public LocalDate getDataSortida() {
+        return null;
+    }
+
+    @Override
+    public void setAllotjament_(Allotjament allotjament_) {
+
+    }
+
+    @Override
+    public void setClient(Client client_) {
+
+    }
+
+    @Override
+    public void setDataEntrada(LocalDate dataEntrada_) {
+
+    }
+
+    @Override
+    public void setDataSortida(LocalDate dataSortida_) {
+
+    }
 }
