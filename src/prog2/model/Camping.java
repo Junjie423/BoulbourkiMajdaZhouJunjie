5995c1b23a1bcc7b9498842cb4fc574eb7635e7a
Junjie423 <jzhouzho423@alumnes.ub.edu>
2026-02-24 13:18:20 +0100

    Initial Versiion

--- src/prog2/model/Camping.java
+++ src/prog2/model/Camping.java
@@ -2,7 +2,19 @@ package prog2.model;
 
 import java.util.ArrayList;
 
-public class Camping {
-
+public class Camping implements InCamping {
+    private String nom;
+    private ArrayList<Allotjament> allojament;
+    private ArrayList<Client> clients;
+    private ArrayList<Reserva> reserves;
+    public Camping(String nom) {
+        this.nom = nom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public LlistaReserves getLlistaReserves() {
+        return reserves;
+    }
 }
 
